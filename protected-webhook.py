d875acbba6121e54488b8deccc4279f7c4dd4d9e
Trevor Dodds <trevor.dodds@gmail.com>
2017-05-02 09:07:29 -0400

    Create protected-webhook.py

---
+++ protected-webhook.py
@@ -0,0 +1,53 @@
+import json
+import os
+import urllib2
+from flask import Flask, request
+
+port = 5000
+githubserver = ''
+api_token = ''
+
+app = Flask(__name__)
+app.debug = os.environ.get('DEBUG') == 'true'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        return "You found me"
+    elif request.method == 'POST':
+        pass
+
+    event = request.headers.get('X-GitHub-Event')
+    if event == 'create':
+        print "we have received a %s event from GHE" % event
+        payload = json.loads(request.data)
+        if 'protected' in payload['ref']:
+            print "Protected Branch - %s - %s" % (payload['repository']['full_name'], payload['ref'])
+            response = invoke_protected(payload)
+            return response
+        else:
+            return "Not a protected branch"
+    else:
+        return "Wrong Event Type"
+
+
+def invoke_protected(data):
+    branch = data['ref']
+    repo_full = data['repository']['full_name']
+    protected = '{"protection": {"enabled": true}}'
+    url = "http://"+githubserver+"/api/v3/repos/"+repo_full+"/branches/"+branch
+    try:
+        print "Calling GitHub API to set %s to protected" % branch
+        req = urllib2.Request(url, data=protected)
+        req.add_header('Authorization', 'token %s' % api_token)
+        req.add_header('Accept', 'application/vnd.github.loki-preview+json')
+        req.get_method = lambda: 'PATCH'
+        f = urllib2.urlopen(req)
+        return f.info()['status']
+    except urllib2.HTTPError, e:
+        print 'We failed with error code - %s.' % e.code
+        return 'We failed with error code - %s.' % e.code
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port) 
